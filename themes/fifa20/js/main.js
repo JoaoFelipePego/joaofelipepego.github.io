39532b2e9d440478b62d2b35ca7caa9aa80c576a
JoaoFelipePego <joaofelipepego1995@hotmail.com>
2019-10-08 00:34:57 -0300

    20191007.v7

--- themes/fifa20/js/main.js
+++ themes/fifa20/js/main.js
@@ -102,6 +102,14 @@ $(document).ready(function() {
 	
 });
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 //***************************
 // by Pego Apps Entertainment
 //**************************/
